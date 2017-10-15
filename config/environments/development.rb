8c13ddeadc592b07146a0d5c2d2d8f78181a4960
Admir23 <hello@admirkurtovic.info>
2017-10-15 14:24:18 +0200

    devise smtp addtional changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,15 +39,15 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    domain: "gmail.com",
+  cconfig.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.gmail.com',
     port: 587,
-    user_name: "jorge@example.org",
-    password: "your_password",
-    authentication: 'plain',
+    domain: 'gmail.com',
+    user_name: 'myemail',
+    password: 'mypass',
+    authentication: :plain,
     enable_starttls_auto: true
   }
 end
