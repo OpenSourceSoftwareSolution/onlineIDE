a266a4fe47c879e144e186e8495cd6a1c343abe2
Renee Sandefur <reneersandefur@gmail.com>
2017-10-30 03:22:14 +0000

    added image to user model and controller, added image size to config

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,9 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    
+      if auth.info.image.present?
+        user.update_attribute(:image, auth.info.image)
+      end
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -274,5 +274,6 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :facebook, Rails.application.secrets.FB_APP_ID, Rails.application.secrets.FB_APP_SECRET, scope: "email", info_fields: "email,name"
+  config.omniauth :facebook, Rails.application.secrets.FB_APP_ID, Rails.application.secrets.FB_APP_SECRET, 
+  scope: "email", info_fields: "email,name", image_size: square
 end
